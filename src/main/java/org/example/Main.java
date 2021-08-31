9a2afa4e9ccfd26330371f22b3173e53893bedaf
Michael <michaeldoyon1@gmail.com>
2021-08-31 16:58:53 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Michael Doyon
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        Scanner Inp = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        String noun = Inp.nextLine();
+        System.out.print("Enter a verb: ");
+        String verb = Inp.nextLine();
+        System.out.print("Enter an adjective: ");
+        String adjective = Inp.nextLine();
+        System.out.print("Enter an adverb: ");
+        String adverb = Inp.nextLine();
+
+        System.out.print("Do you " + verb + " your " + adjective + " " + noun + " " + adverb + "? That's hilarious!");
+    }
+}
